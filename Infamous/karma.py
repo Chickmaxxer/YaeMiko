55b8ef823b3970f0b2c590d8a619423bbed06b3f
Pruthvi Lalasangi <pruthvilalasangi8@gmail.com>
2024-05-18 20:45:12 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -12,7 +12,7 @@ START_IMG = [
     "https://graph.org/file/eac99dd18a261b11adaca.jpg",
     "https://graph.org/file/8c924fd0844c6cadedd07.jpg",
     "https://graph.org/file/4d8f6f38a7f3683fa2a4a.jpg",
-    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
+    "https://graph.org/file/67979fc64642071b7e8e3.jpg",
     "https://telegra.ph/file/262c86393730a609cdade.jpg",
     "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
 ]
